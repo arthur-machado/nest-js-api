50109493caadeb69dced03b976a03b71452bb2a3
arthur-machado <arthurmachado7@gmail.com>
2022-04-12 10:48:55 -0300

    refactor: remove tracks dangerous routes

--- src/components/tracks/tracks.controller.ts
+++ src/components/tracks/tracks.controller.ts
@@ -1,15 +1,6 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { TracksService } from './tracks.service';
 import { CreateTrackDto } from './dto/create-track.dto';
-import { UpdateTrackDto } from './dto/update-track.dto';
 
 @Controller('tracks')
 export class TracksController {
@@ -20,23 +11,8 @@ export class TracksController {
     return this.tracksService.create(createTrackDto);
   }
 
-  @Get()
-  findAll() {
-    return this.tracksService.findAll();
-  }
-
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.tracksService.findOne(id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTrackDto: UpdateTrackDto) {
-    return this.tracksService.update(+id, updateTrackDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.tracksService.remove(+id);
   }
 }
